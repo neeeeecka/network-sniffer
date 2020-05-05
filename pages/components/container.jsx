adfb6040200ed2ff55e57c4366f1326d5b64e9de
blackstormx <blackshtormx@gmail.com>
2020-05-05 20:06:12 +0400

    css

--- pages/components/container.jsx
+++ pages/components/container.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Container extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="w-1/2 p-2 bg-gray-900 items-center text-indigo-100 lg:rounded-full shadow-inner p-3 cursor-pointer">
+        <div className="bg-indigo-800 p-2 rounded">
+          <div className=""></div>
+        </div>
+      </div>
+    );
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Container from "./components/container";
 
 const index = () => (
-  <div className="bg-gray-800 h-screen">
-    <Container />
+  <div className="bg-gray-800 h-screen p-2 mb-4">
+    <div className="flex">
+      <Container />
+      <Container />
+    </div>
   </div>
 );
 
