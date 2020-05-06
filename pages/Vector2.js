dd368db834c0b7d1e595c2761c6d5e6fcb3fc6bf
blackstormx <blackshtormx@gmail.com>
2020-05-06 18:10:31 +0400

    vectors

--- pages/Vector2.js
+++ pages/Vector2.js
@@ -3,4 +3,10 @@ class Vector2 {
     this.x = x;
     this.y = y;
   }
+  add(vec2) {
+    return new Vector2(this.x + vec2.x, this.y + vec2.y);
+  }
+  subtract(vec2) {
+    return new Vector2(this.x - vec2.x, this.y - vec2.y);
+  }
 }
