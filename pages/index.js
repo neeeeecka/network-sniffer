04dc2b1f92c8b5e4a92d1a39042b205eb5099577
blackstormx <blackshtormx@gmail.com>
2020-10-01 23:19:41 +0400

    save

--- pages/index.js
+++ pages/index.js
@@ -27,86 +27,12 @@ class Index extends Component {
       // }
     ]
   };
-  updateUnitType = async (_id, type, sortIndex) => {
-    const newUnits = [...this.state.units];
 
-    const changes = { type: type };
-    const result = await this.fetchEditUnit(_id, changes);
-    if (!result.error) {
-      const unitsOfType = newUnits.filter(unit => unit.data.type === type);
-
-      const selectedUnit = newUnits.find(unit => {
-        return unit.data._id === _id;
-      });
-      const otherUnits = newUnits.filter(
-        unit => !unitsOfType.includes(unit) && unit !== selectedUnit
-      );
-
-      if (type === selectedUnit.data.type) {
-        array_move(unitsOfType, unitsOfType.indexOf(selectedUnit), sortIndex);
-      } else {
-        unitsOfType.splice(sortIndex, 0, selectedUnit);
-      }
-
-      const combined = unitsOfType.concat(otherUnits);
-
-      selectedUnit.data.type = type;
-      selectedUnit.el = null;
-
-      this.setState({ units: combined });
-    }
-  };
-  onUnitInit = (_id, el) => {
-    const newUnits = [...this.state.units];
-    const found = newUnits.find(unit => unit.data._id === _id);
-    found.el = el;
-    this.setState({ units: newUnits });
-  };
-
-  fetchUnits = async () => {
-    let response = await fetch(`${cURL}/units`, {
-      method: "GET"
-    });
-    let data = await response.json();
-    const units = [];
-    data.forEach(unit => {
-      units.push({ data: unit, el: undefined });
-    });
-    this.setState({ units: units });
-  };
-  fetchEditUnit = async (_id, changes) => {
-    let response = await fetch(`${cURL}/units/${_id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(changes)
-    });
-    let data = await response.json();
-    return data;
-  };
-
-  componentDidMount = async () => {
-    this.fetchUnits();
-  };
+  componentDidMount = async () => {};
   render() {
     return (
       <div className="bg-gray-800 h-screen  overflow-hidden">
-        <div
-          id="rectDebugger"
-          style={{
-            overflow: "hidden",
-            width: "100%",
-            height: "100%",
-            position: "absolute"
-          }}
-        />
         <Header />
-        <Units
-          units={this.state.units}
-          updateUnitType={this.updateUnitType}
-          onUnitInit={this.onUnitInit}
-        />
       </div>
     );
   }
